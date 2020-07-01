1ce89015efe404feeda3098c9011a2a4adc47472
frederic jonquieres <fjonquieres83@gmail.com>
2020-07-01 14:53:31 +0100

    RST-2603 GOVUK Submit button

--- lib/et_test_helpers/capybara/govuk_submit_selector.rb
+++ lib/et_test_helpers/capybara/govuk_submit_selector.rb
@@ -2,6 +2,6 @@ Capybara.add_selector(:govuk_submit) do
   xpath do |locator, **options|
     locator = ::EtTestHelpers.normalize_locator(locator)
     # Helps with nicer error messages from rspec etc..
-    XPath.generate { |x| x.descendant(:input)[x.attr(:class).contains_word('govuk-button')] }
+    XPath.generate { |x| x.descendant(:input)[x.attr(:class).contains_word('govuk-button') & x.attr(:value).equals(locator)] }
   end
 end
