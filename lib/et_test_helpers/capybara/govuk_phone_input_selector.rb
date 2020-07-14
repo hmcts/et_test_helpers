284db50047f16458981a3e5dea3d1919e0305067
Gary Taylor <gary.taylor@hismessages.com>
2020-07-14 08:42:44 +0100

    Added value method to phone field

--- lib/et_test_helpers/capybara/govuk_phone_input_selector.rb
+++ lib/et_test_helpers/capybara/govuk_phone_input_selector.rb
@@ -1,4 +1,4 @@
-Capybara.add_selector(:govuk_email_input) do
+Capybara.add_selector(:govuk_phone_input) do
   label 'GOVUK GDS email input'
   xpath do
     XPath.generate { |x| x.descendant(:input)[x.attr(:class).contains_word('govuk-input') & x.attr(:type).equals('tel') & x.attr(:autocomplete).equals('tel')] }
