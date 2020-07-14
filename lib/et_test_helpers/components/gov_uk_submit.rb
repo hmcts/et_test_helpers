10630a0593a4efadaba92bd4c3d5d47c31c688cd
Gary Taylor <gary.taylor@hismessages.com>
2020-07-14 09:45:00 +0100

    Added click to govuk submit section

--- lib/et_test_helpers/components/gov_uk_submit.rb
+++ lib/et_test_helpers/components/gov_uk_submit.rb
@@ -2,7 +2,7 @@ module EtTestHelpers
   module Components
     # A gov.uk GDS standard submit button representation for testing
     class GovUKSubmit < ::SitePrism::Section
-      def click
+      def submit
         root_element.click
       end
     end
