8fc3c9160ba9e09afb2f3ce5782120a2a5e2e6cc
Gary Taylor <gary.taylor@hismessages.com>
2020-07-20 11:21:54 +0100

    Added with node filters to email and phone fields

--- lib/et_test_helpers/capybara/govuk_phone_field_selector.rb
+++ lib/et_test_helpers/capybara/govuk_phone_field_selector.rb
@@ -8,4 +8,15 @@ Capybara.add_selector(:govuk_phone_field) do
     field_xpath = locate_field(xpath, locator, **options)
     XPath.generate {|x| x.css('.govuk-form-group')[field_xpath] }
   end
+
+  node_filter(:with) do |node, with|
+    val = node.value
+    (with.is_a?(Regexp) ? with.match?(val) : val == with.to_s).tap do |res|
+      add_error("Expected value to be #{with.inspect} but was #{val.inspect}") unless res
+    end
+  end
+
+  describe_node_filters do |**options|
+    " with value #{options[:with].to_s.inspect}" if options.key?(:with)
+  end
 end
