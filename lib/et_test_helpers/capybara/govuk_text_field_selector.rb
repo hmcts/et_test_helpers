cdec043746cf6b970fefa717fa4b155c1c5f39e8
Gary Taylor <gary.taylor@hismessages.com>
2020-07-20 10:45:14 +0100

    Added assert_value method to text field

--- lib/et_test_helpers/capybara/govuk_text_field_selector.rb
+++ lib/et_test_helpers/capybara/govuk_text_field_selector.rb
@@ -15,4 +15,8 @@ Capybara.add_selector(:govuk_text_field) do
       add_error("Expected value to be #{with.inspect} but was #{val.inspect}") unless res
     end
   end
+
+  describe_node_filters do |**options|
+    " with value #{options[:with].to_s.inspect}" if options.key?(:with)
+  end
 end
