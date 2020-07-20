8825836ca62d7df3143a6ea5ff69b9ba76170796
Gary Taylor <gary.taylor@hismessages.com>
2020-07-20 10:50:21 +0100

    Added assert_value method to text field

--- lib/et_test_helpers/capybara/govuk_text_field_selector.rb
+++ lib/et_test_helpers/capybara/govuk_text_field_selector.rb
@@ -8,15 +8,4 @@ Capybara.add_selector(:govuk_text_field) do
     field_xpath = locate_field(xpath, locator, **options)
     XPath.generate {|x| x.css('.govuk-form-group')[field_xpath] }
   end
-
-  node_filter(:with) do |node, with|
-    val = node.value
-    (with.is_a?(Regexp) ? with.match?(val) : val == with.to_s).tap do |res|
-      add_error("Expected value to be #{with.inspect} but was #{val.inspect}") unless res
-    end
-  end
-
-  describe_node_filters do |**options|
-    " with value #{options[:with].to_s.inspect}" if options.key?(:with)
-  end
 end
