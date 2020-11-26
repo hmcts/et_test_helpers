21b0fdbee14f72a32bc0955e6ee2111831463e50
Gary Taylor <gary.taylor@hismessages.com>
2020-11-26 06:55:19 +0000

    Submit capybara selector did not show correct error message when not found

--- lib/et_test_helpers/capybara/govuk_submit_selector.rb
+++ lib/et_test_helpers/capybara/govuk_submit_selector.rb
@@ -1,7 +1,9 @@
 Capybara.add_selector(:govuk_submit) do
+  label "GOVUK submit button labelled"
   xpath do |locator, **options|
     locator = ::EtTestHelpers.normalize_locator(locator)
     # Helps with nicer error messages from rspec etc..
+    @definition.label("GOVUK submit button labelled <#{locator}>")
     XPath.generate { |x| x.descendant(:input)[x.attr(:class).contains_word('govuk-button') & x.attr(:value).equals(locator)] }
   end
 end
