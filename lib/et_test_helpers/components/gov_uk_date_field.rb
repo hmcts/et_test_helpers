141e52518a3a5c134d8baee16207a617b4a46cf0
Gary Taylor <gary.taylor@hismessages.com>
2021-03-12 19:14:15 +0000

    Added disabled? method to date

--- lib/et_test_helpers/components/gov_uk_date_field.rb
+++ lib/et_test_helpers/components/gov_uk_date_field.rb
@@ -75,7 +75,7 @@ module EtTestHelpers
         end
       end
 
-      delegate [:assert_value, :assert_error_message, :set, :value, :label, :hint, :error, :has_no_error?, :has_no_hint?] => :fieldset
+      delegate [:assert_value, :assert_error_message, :set, :value, :label, :hint, :error, :has_no_error?, :has_no_hint?, :disabled?] => :fieldset
     end
   end
 end
