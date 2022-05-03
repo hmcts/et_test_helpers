997c95fc27507b11fb5b5e42a1affef7b695e014
Gary Taylor <gary.taylor@hismessages.com>
2022-05-03 08:01:14 +0100

    Added govuk_link selector

---
+++ lib/et_test_helpers/capybara/govuk_link_selector.rb
@@ -0,0 +1,10 @@
+Capybara.add_selector(:govuk_link) do
+  label 'GOVUK GDS Link labelled'
+  xpath do |locator, translation_options: {}, **options|
+    locator = ::EtTestHelpers.normalize_locator(locator, translation_options: translation_options)
+    locator = locator[:label] if locator.is_a?(Hash)
+    # Helps with nicer error messages from rspec etc..
+    @definition.label("GOVUK GDS Link labelled <#{locator}>")
+    XPath.generate { |x| x.descendant(:a)[x.attr(:class).contains_word('govuk-link')[x.string.n.equals(locator)]] }
+  end
+end
