63bc6abdb95c7144da45a61bd9996ae1c8a8bfcc
Gary Taylor <gary.taylor@hismessages.com>
2022-05-03 10:11:46 +0100

    Fixed bug in govuk_button_link selector

--- lib/et_test_helpers/capybara/govuk_button_link_selector.rb
+++ lib/et_test_helpers/capybara/govuk_button_link_selector.rb
@@ -5,6 +5,6 @@ Capybara.add_selector(:govuk_button_link) do
     locator = locator[:label] if locator.is_a?(Hash)
     # Helps with nicer error messages from rspec etc..
     @definition.label("GOVUK GDS Button Link labelled <#{locator}>")
-    XPath.generate { |x| x.descendant(:a)[x.attr(:class).contains_word('govuk-link') & x.attr(:role).equals('button') & x.string.n.equals(locator)] }
+    XPath.generate { |x| x.descendant(:a)[x.attr(:class).contains_word('govuk-button') & x.attr(:role).equals('button') & x.attr(:'data-module').equals('govuk-button') & x.string.n.equals(locator)] }
   end
 end
