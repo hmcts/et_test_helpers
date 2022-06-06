e9db876106aaf13fcec03db2d1c6a6088e3cebaf
Gary Taylor <gary.taylor@hismessages.com>
2022-06-06 07:53:57 +0100

    Relaxed specs for error elements - will now work with latest version of et_gds_design_system

--- lib/et_test_helpers/capybara/govuk_field_error_selector.rb
+++ lib/et_test_helpers/capybara/govuk_field_error_selector.rb
@@ -1,6 +1,6 @@
 Capybara.add_selector(:govuk_field_error) do
   label 'GOVUK GDS field error'
   xpath do
-    XPath.generate { |x| x.descendant(:span)[x.attr(:class).contains_word('govuk-error-message') & x.child(:span)[x.attr(:class).contains_word('govuk-visually-hidden') & x.string.n.equals('Error:')]] }
+    XPath.generate { |x| x.descendant[x.attr(:class).contains_word('govuk-error-message') & x.child[x.attr(:class).contains_word('govuk-visually-hidden') & x.string.n.equals('Error:')]] }
   end
 end
